f0c36768ee3834bd67d4165655cda20333fa283d
Camilo Cabuya Berrio <cabuyaberriojin@gmail.com>
2024-08-15 21:28:46 -0500

    accordion

---
+++ main.js
@@ -0,0 +1,38 @@
+const plusButtons = document.querySelectorAll(".plus-btn");
+
+// contenedor de preguntas
+const questionsContainer = document.querySelector(".questions");
+
+// contenedor del texto
+const textContainer = document.querySelectorAll(".question-answer");
+
+// desplegar
+const desplegar = (button) => {
+    // contenedor respuesta
+    const answer = button.closest(".question").lastElementChild;
+
+    plusButtons.forEach((btn) => {
+        if (button !== btn) {
+            if (btn.classList.contains("plus-btn--active")) {
+                btn.classList.remove("plus-btn--active");
+                btn.closest(".question").lastElementChild.classList.remove(
+                    "question-answer--active"
+                );
+            }
+        }
+    });
+
+    button.classList.toggle("plus-btn--active");
+    answer.classList.toggle("question-answer--active");
+};
+
+// ocultar
+
+// eventos mediante delegacion
+questionsContainer.addEventListener("click", (e) => {
+    if (e.target.matches("i.plus-btn")) {
+        // contenedor de la respuesta
+
+        desplegar(e.target);
+    }
+});
